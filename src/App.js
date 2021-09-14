11bbfba80452604d4e5729760d332e51832b0306
Liana Baryshova <barishovaliana@gmail.com>
2021-09-14 22:32:56 +0200

    Add Component Modal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
 import { Component } from 'react';
-import Searchbar from './components/Searchbar';
+import Searchbar from './components/Searchbar/Searchbar.jsx';
+import fetchImages from './services/pixabay-api';
+import ImageGallery from './components/ImageGallery/ImageGallery.jsx';
+import Button from './components/Button/Button.jsx';
+import Modal from './components/Modal/Modal.jsx';
 
 export default class App extends Component {
   state = {
     imageName: '',
+    images: [],
+    reqStatus: 'idle',
+    page: 1,
+    showModal: false,
+    modalImage: null,
   };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const nextQuery = this.state.imageName;
+    const nextPage = this.state.page;
+
+    if (prevState.imageName !== nextQuery || prevState.page !== nextPage) {
+      const images = await fetchImages(nextQuery, nextPage);
+      this.setState({ images });
+    }
+  }
 
   handleFormSubmit = imageName => {
     this.setState({ imageName });
   };
 
+  loadMoreBtn = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  handleSelectedImage = largeImage => {
+    this.setState(prevState => ({
+      showModal: !prevState.showModal,
+      modalImage: largeImage,
+    }));
+  };
+
   render() {
+    const { images, showModal, modalImage } = this.state;
+
+    const showButton = images.length >= 1;
     return (
       <div>
-        <Searchbar onSubmit={this.handleFormSubmit} />
+        <Searchbar onSearch={this.handleFormSubmit} />
+        <ImageGallery
+          images={images}
+          toggleModal={this.toggleModal}
+          largeImage={this.handleSelectedImage}
+        />
+        {showButton && <Button onClick={this.loadMoreBtn} />}
+        {showModal && <Modal modalImage={modalImage} onClose={this.toggleModal} />}
       </div>
     );
   }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
 
-class componentName extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
     }
+  };
+
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    const { largeImageURL, tags } = this.props.modalImage;
+    // console.log(this.props.modalImage);
+
+    return createPortal(
+      <div className="Overlay" onClick={this.handleBackdropClick}>
+        <div className="Modal">
+          <img
+            src={largeImageURL}
+            alt={tags}
+          />
+        </div>
+      </div>,
+      modalRoot
+    );
+
+  };
 }
 
-export default componentName;
+export default Modal;
 
-<div className="Overlay">
-  <div className="Modal">
-    <img src="" alt="" />
-  </div>
-</div>
